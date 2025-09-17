73b94d4b0e1ab9c88ba78b3ad1b20fe951f4f1e0
shrinemtg <shrine.mtg@gmail.com>
2025-09-17 19:49:07 +0900

    ファビコンの修正

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/favicon.ico',
+        destination: '/fabikon/favicon-32.ico',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
